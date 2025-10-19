eafb679be03ff3a774c452b4e9cc0a499c1625fa
einfachesache <klugetommy@web.de>
2025-10-19 20:58:08 +0200

    Changes -> added whitelist for all Servers

--- src/main/java/de/einfachesache/proxymanager/velocity/listener/LoginAccessControlListener.java
+++ src/main/java/de/einfachesache/proxymanager/velocity/listener/LoginAccessControlListener.java
@@ -23,7 +23,8 @@ public class LoginAccessControlListener {
     private static final String EVENT_SERVER = "Event";
     private static final String FALLBACK_SERVER = "Limbo";
     private static final String BYPASS_PERMISSION_MAINTENANCE = "proxy.bypass.maintenance";
-    private static final Component DENY_MESSAGE = Component.text("⛔ Du bist fürs Event nicht whitelisted » /whitelist im Discord", NamedTextColor.RED);
+    private static final Component DENY_MESSAGE = Component.text("⛔ Du bist nicht whitelisted » /whitelist im Discord", NamedTextColor.RED);
+    private static final Component EVENT_DENY_MESSAGE = Component.text("⛔ Du bist fürs Event nicht whitelisted » /whitelist im Discord", NamedTextColor.RED);
 
     private static VProxyManager proxy;
 
@@ -44,21 +45,25 @@ public class LoginAccessControlListener {
 
     @Subscribe
     public void onServerPreConnect(ServerPreConnectEvent event) {
-        if (!Config.isEventWhitelist()) return;
-        if (!event.getOriginalServer().getServerInfo().getName().equalsIgnoreCase(EVENT_SERVER)) return;
 
         var player = event.getPlayer();
+        var originalServer = event.getOriginalServer();
+
+        if (!Config.isEventWhitelist()) return;
         if (hasWhitelistAccess(player)) return;
+        if (originalServer.getServerInfo().getName().equalsIgnoreCase(FALLBACK_SERVER)) return;
+
+        Component denyMessage = originalServer.getServerInfo().getName().toLowerCase().contains(EVENT_SERVER.toLowerCase()) ? EVENT_DENY_MESSAGE : DENY_MESSAGE;
 
         if (event.getPreviousServer() != null) {
             event.setResult(ServerPreConnectEvent.ServerResult.denied());
-            player.sendMessage(DENY_MESSAGE);
+            player.sendMessage(denyMessage);
             return;
         }
 
         Optional<RegisteredServer> fallback = proxy.getProxy().getServer(FALLBACK_SERVER);
         if (fallback.isEmpty()) {
-            player.disconnect(DENY_MESSAGE);
+            player.disconnect(denyMessage);
             return;
         }
 
@@ -88,7 +93,7 @@ public class LoginAccessControlListener {
 
         Optional<RegisteredServer> fallback = proxy.getProxy().getServer(FALLBACK_SERVER);
         if (fallback.isEmpty()) {
-            player.disconnect(DENY_MESSAGE);
+            player.disconnect(EVENT_DENY_MESSAGE);
             return;
         }
 
