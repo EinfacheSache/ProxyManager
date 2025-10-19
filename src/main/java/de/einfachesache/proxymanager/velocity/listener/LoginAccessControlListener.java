3fac2d8495604c04a292c5c422c16003bd0438a7
einfachesache <klugetommy@web.de>
2025-10-19 13:37:02 +0200

    Changes -> removed whitelist bypass role

--- src/main/java/de/einfachesache/proxymanager/velocity/listener/LoginAccessControlListener.java
+++ src/main/java/de/einfachesache/proxymanager/velocity/listener/LoginAccessControlListener.java
@@ -6,6 +6,7 @@ import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import de.einfachesache.proxymanager.core.Config;
+import de.einfachesache.proxymanager.core.Core;
 import de.einfachesache.proxymanager.velocity.ScreenBuilder;
 import de.einfachesache.proxymanager.velocity.VProxyManager;
 import net.kyori.adventure.text.Component;
@@ -13,13 +14,13 @@ import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 public class LoginAccessControlListener {
 
     private static final String EVENT_SERVER = "Event";
     private static final String FALLBACK_SERVER = "Limbo";
-    private static final String BYPASS_PERMISSION_WHITELIST = "proxy.bypass.whitelist";
     private static final String BYPASS_PERMISSION_MAINTENANCE = "proxy.bypass.maintenance";
     private static final Component DENY_MESSAGE = Component.text("⛔ Du bist fürs Event nicht whitelisted » /whitelist im Discord", NamedTextColor.RED);
 
@@ -64,7 +65,7 @@ public class LoginAccessControlListener {
     }
 
     public static boolean hasWhitelistAccess(Player player) {
-        if (player.hasPermission(BYPASS_PERMISSION_WHITELIST)) return true;
+        if (Objects.equals(player.getUniqueId(), Core.DEV_UUID)) return true;
         Map<String, String> wl = Config.getWhitelistedPlayers();
         return wl != null && wl.values().stream().anyMatch(name -> name.equalsIgnoreCase(player.getUsername()));
     }
