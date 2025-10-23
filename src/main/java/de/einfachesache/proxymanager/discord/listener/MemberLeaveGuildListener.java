1cf7a36d8ce0b0bc4900de5223119d8218213b9b
einfachesache <klugetommy@web.de>
2025-10-23 14:20:15 +0200

    Changes -> reworked MemberLeaveGuildListener

--- src/main/java/de/einfachesache/proxymanager/discord/listener/MemberLeaveGuildListener.java
+++ src/main/java/de/einfachesache/proxymanager/discord/listener/MemberLeaveGuildListener.java
@@ -1,6 +1,7 @@
 package de.einfachesache.proxymanager.discord.listener;
 
 import de.einfachesache.proxymanager.core.Config;
+import de.einfachesache.proxymanager.core.Core;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,6 +10,7 @@ public class MemberLeaveGuildListener extends ListenerAdapter {
     @Override
     public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
         if (!Config.getGuildIDs().contains(event.getGuild().getId())) return;
+        if (Core.getDiscordAPI().getGuilds().values().stream().anyMatch(guild -> guild.getMemberById(event.getUser().getId()) != null)) return;
 
         Config.removeFromWhitelistByUser(event.getUser().getId());
     }
